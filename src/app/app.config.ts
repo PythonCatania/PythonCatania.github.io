d269a093a0f918f6ac04e7ec87367d00a3635bcc
Helias <stefanoborzi32@gmail.com>
2026-03-15 13:04:16 +0100

    fix: base href references

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
+import { DOCUMENT } from '@angular/common';
+import { IMAGE_LOADER, ImageLoaderConfig } from '@angular/common';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    
-  ]
+    {
+      provide: IMAGE_LOADER,
+      useFactory: (document: Document) => (config: ImageLoaderConfig): string => {
+        const base = document.querySelector('base')?.getAttribute('href') ?? '/';
+        const baseNormalized = base.endsWith('/') ? base.slice(0, -1) : base;
+        return baseNormalized + config.src;
+      },
+      deps: [DOCUMENT],
+    },
+  ],
 };
