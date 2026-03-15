57299b13503305395d7e853fc00c86d74c8f8b96
Helias <stefanoborzi32@gmail.com>
2026-03-16 00:40:21 +0100

    feat: update chi siamo page

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -1,22 +1,27 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { TranslocoModule } from '@jsverse/transloco';
+import { IconGithubComponent } from '../../components/shared/icons/icon-github/icon-github.component';
+import { IconLinkedinComponent } from '../../components/shared/icons/icon-linkedin/icon-linkedin.component';
 
 interface TeamMember {
   readonly name: string;
+  readonly image: string;
   readonly github?: string;
+  readonly linkedin?: string;
 }
 
 @Component({
   selector: 'app-team',
-  imports: [TranslocoModule],
+  imports: [TranslocoModule, NgOptimizedImage, IconGithubComponent, IconLinkedinComponent],
   templateUrl: './team.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TeamComponent {
   protected readonly members: readonly TeamMember[] = [
-    { name: 'Salvatore Rapisarda', github: 'https://github.com/salvo-rapisarda' },
-    { name: 'Matteo Jacopo Schembri', github: 'https://github.com/mjschembri' },
-    { name: 'Stefano Borzi' },
-    { name: 'Claudio Faro' },
+    { name: 'Salvatore Rapisarda', image: '/images/about/SalvoRapisarda.jpeg', github: 'https://github.com/salvorapi', linkedin: 'https://www.linkedin.com/in/salvorapi/' },
+    { name: 'Matteo Jacopo Schembri', image: '/images/about/MatteoJacopoSchembri.jpeg', github: 'https://github.com/teosche', linkedin: 'https://www.linkedin.com/in/teosche/' },
+    { name: 'Stefano Borzi', image: '/images/about/StefanoBorzi.jpeg', github: 'https://github.com/Helias', linkedin: 'https://www.linkedin.com/in/stefanoborzi/' },
+    { name: 'Claudio Faro', image: '/images/about/ClaudioFaro.jpeg', github: 'https://github.com/kaesarct', linkedin: 'https://www.linkedin.com/in/claudio-faro-70128649/' },
   ];
 }
